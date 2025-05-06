b50c6c9b79c290b03ab5bff77177c6a2272fd9cf
sweetclimusic <4057640+sweetclimusic@users.noreply.github.com>
2025-05-06 11:33:50 +0100

    Initial Commit

---
+++ composeHostingApp/composeHostingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  composeHostingAppApp.swift
+//  composeHostingApp
+//
+//  Created by Ashlee Muscroft on 06/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct composeHostingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ composeHostingAppTests/composeHostingAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  composeHostingAppTests.swift
+//  composeHostingAppTests
+//
+//  Created by Ashlee Muscroft on 06/05/2025.
+//
+
+import Testing
+@testable import composeHostingApp
+
+struct composeHostingAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
